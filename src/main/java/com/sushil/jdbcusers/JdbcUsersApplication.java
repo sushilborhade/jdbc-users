88fc0bb5648a6cb7c36ebb15b802ef17b7e7d80c
Sushil P. Borhade <sborhade@tetravx.com>
2024-03-05 15:32:09 +0530

    Initial commit

---
+++ src/main/java/com/sushil/jdbcusers/JdbcUsersApplication.java
@@ -0,0 +1,13 @@
+package com.sushil.jdbcusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sushil/jdbcusers/JdbcUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sushil.jdbcusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
